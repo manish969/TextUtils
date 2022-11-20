cb29e1b98807e0641780284b49e9df38e8614499
Manish <manishcutm2001@gmail.com>
2022-11-20 23:24:44 +0530

    Updates

---
+++ precache-manifest.bcf9cf24f0d2104740b6acf5208bdeda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7867de904b7431a17774",
+    "url": "/TextUtils/static/css/main.a98e32b0.chunk.css"
+  },
+  {
+    "revision": "7867de904b7431a17774",
+    "url": "/TextUtils/static/js/main.7867de90.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/TextUtils/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/TextUtils/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "05b54db4a4c3678b5054",
+    "url": "/TextUtils/static/js/runtime~main.05b54db4.js"
+  },
+  {
+    "revision": "6445a0db820e4472a7c2b0ece373b8fb",
+    "url": "/TextUtils/index.html"
+  }
+];
